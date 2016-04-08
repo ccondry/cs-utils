0ff67ac479775df81a472c9688f92ffa481d8593
tdippel <tdippel@cisco.com>
2016-04-08 15:05:33 -0400

    Updated with new default user and added flag to include kms scopes

--- bin/token.js
+++ bin/token.js
@@ -3,11 +3,22 @@
 var auth = require("../index.js").Auth,
 	chalk = require("chalk"),
 	argv = require('minimist')(process.argv.slice(2));
+
+var KMS_SCOPES = 'webex-squared:kms_read webex-squared:kms_bind webex-squared:kms_write';
+
 if(argv.help ){
-	console.log(chalk.yellow("Usage: cs-token [--user] [--password] [--orgid] [--help]"));
+	console.log(chalk.yellow("Usage: cs-token [--user] [--password] [--orgid] [--kms] [--help]"));
 	return;
 }
-auth.getToken(argv.user,argv.password,argv.orgid)
+
+var kmsScopes;
+if (argv.kms || argv.kms === 'true') {
+	kmsScopes = KMS_SCOPES;
+} else {
+	kmsScopes = null;
+}
+
+auth.getToken(argv.user,argv.password,argv.orgid,kmsScopes)
 .then(function(token){
 	console.log(chalk.gray("************************ ACCESS TOKEN ************************"));
 	console.log(chalk.green(token.access_token))
