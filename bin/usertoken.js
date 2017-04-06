7003c288d6e3ec9688c6af11f9d6fc87190385d8
anitin <anitin@cisco.com>
2017-04-06 19:49:05 -0400

    adding user token command to get user token for dev testing

---
+++ bin/usertoken.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+'use strict';
+var auth = require("../index.js").Auth,
+	chalk = require("chalk"),
+	argv = require('minimist')(process.argv.slice(2));
+
+if(argv.help ){
+	console.log(chalk.yellow("Usage: cs-user-token [--user] [--password] [--orgid][--help]"));
+	return;
+}
+var SCOPE = "contact-center-context:pod_write contact-center-context:pod_read webex-squared:kms_read webex-squared:kms_bind webex-squared:kms_write spark:kms";
+
+auth.getOrgAdminToken(argv.user,argv.password,argv.orgid,SCOPE)
+.then(function(token){
+	console.log(chalk.gray("************************ ACCESS TOKEN ************************"));
+	console.log(chalk.green(token.access_token))
+	console.log(chalk.gray("**************************************************************"));
+	console.log(chalk.italic("CLIENT ID:"),chalk.cyan(auth.USER_CLIENT_ID));
+	console.log(chalk.italic("CLIENT SECRET:"),chalk.cyan(auth.USER_CLIENT_SECRET));
+})
+.catch(function(e){
+	console.error("Cannot get token for user : ",argv.user," for org :",argv.orgid);
+	console.error("Error occured",e.data? e.data.error: e);
+});
