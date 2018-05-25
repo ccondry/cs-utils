3b641e7eac975583568d7b3c6852c7256a5cbc24
Coty Condry <ccondry@cisco.com>
2018-05-25 02:04:30 -0500

    rewrite refreshAccessToken method modularize some functions out of getAccessToken

--- test/test.js
+++ test/test.js
@@ -10,35 +10,12 @@ const kmsScopes = true
 
 const cache = {}
 
-describe('auth.decodeConnectionData(connectionData)', function () {
-  it('should decode connection data string into JSON', function (done) {
-    // const buff = new Buffer(connectionData, 'base64')
-    // const text = buff.toString('ascii')
-    // const json = JSON.parse(text)
-    // const credentials = labMode ? json.credentials : json.credentialsLabMode
-    // console.log('orgId', credentials.orgId)
-    // auth.getToken(credentials.name, credentials.password, credentials.orgId, kmsScopes)
-    // .then(rsp => {
-    //   console.log('got access token:', rsp)
-    //   cache.credentials = credentials
-    //   cache.token = rsp
-    //   done()
-    // })
-    // .catch(e => {
-    //   done(e)
-    // })
-    const json = auth.decodeConnectionData(connectionData)
-    console.log('decoded connection data', json)
-    cache.connectionData = json
-    done()
-  })
-})
-
-describe('getAccessToken(connectionData, labMode, includeKmsScopes)', function () {
-  it('should get access token', function (done) {
-    auth.getAccessToken(connectionData, true, true)
-    .then(rsp => {
-      console.log('access token:', rsp)
+describe('auth.getOrgAdminToken(username, password, orgId, scopes)', function () {
+  it('should get org admin token', function (done) {
+    this.timeout(4000)
+    auth.getOrgAdminToken(orgEmail, orgPassword, orgId)
+    .then(response => {
+      console.log('getOrgAdminToken', response)
       done()
     })
     .catch(e => {
@@ -47,17 +24,39 @@ describe('getAccessToken(connectionData, labMode, includeKmsScopes)', function (
   })
 })
 
+describe('auth.decodeConnectionData(connectionDataString)', function () {
+  it('should decode connection data string into JSON', function (done) {
+    const connectionData = auth.decodeConnectionData(connectionDataString)
+    console.log('decoded connection data', connectionData)
+    cache.connectionData = connectionData
+    done()
+  })
+})
 
-// describe('refresh access token', function () {
-//   it('should refresh the access token received in the previous test', function (done) {
-//     // auth.refreshToken(cache.credentials, cache.token)
-//     auth.refreshToken(cache.credentials, cache.token)
-//     .then(rsp => {
-//       console.log('refreshed access token:', rsp)
-//       done()
-//     })
-//     .catch(e => {
-//       done(e)
-//     })
-//   })
-// })
+describe('auth.getAccessToken(connectionData, labMode, includeKmsScopes)', function () {
+  it('should get access token', function (done) {
+    auth.getAccessToken(connectionDataString, true, true)
+    .then(rsp => {
+      console.log('access token:', rsp)
+      cache.accessToken = rsp
+      done()
+    })
+    .catch(e => {
+      done(e)
+    })
+  })
+})
+
+describe('refresh access token', function () {
+  it('should refresh the access token received in the previous test', function (done) {
+    // auth.refreshToken(cache.credentials, cache.token)
+    auth.refreshAccessToken(connectionDataString, true, cache.accessToken)
+    .then(rsp => {
+      console.log('refreshed access token:', rsp)
+      done()
+    })
+    .catch(e => {
+      done(e)
+    })
+  })
+})
