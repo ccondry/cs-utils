40ff46cde52eec7e5053ac1bce4d1e1d56b3f222
Coty Condry <ccondry@cisco.com>
2018-05-25 01:14:57 -0500

    start adding mocha tests fix whitespace organize code simplify HTTP request code

---
+++ test/test.js
@@ -0,0 +1,59 @@
+const auth = require("../index").Auth
+
+const connectionData = ``
+const labMode = true
+const kmsScopes = true
+
+const cache = {}
+
+describe('auth.decodeConnectionData(connectionData)', function () {
+  it('should decode connection data string into JSON', function (done) {
+    // const buff = new Buffer(connectionData, 'base64')
+    // const text = buff.toString('ascii')
+    // const json = JSON.parse(text)
+    // const credentials = labMode ? json.credentials : json.credentialsLabMode
+    // console.log('orgId', credentials.orgId)
+    // auth.getToken(credentials.name, credentials.password, credentials.orgId, kmsScopes)
+    // .then(rsp => {
+    //   console.log('got access token:', rsp)
+    //   cache.credentials = credentials
+    //   cache.token = rsp
+    //   done()
+    // })
+    // .catch(e => {
+    //   done(e)
+    // })
+    const json = auth.decodeConnectionData(connectionData)
+    console.log('decoded connection data', json)
+    cache.connectionData = json
+    done()
+  })
+})
+
+describe('getAccessToken(connectionData, labMode, includeKmsScopes)', function () {
+  it('should get access token', function (done) {
+    auth.getAccessToken(connectionData, true, true)
+    .then(rsp => {
+      console.log('access token:', rsp)
+      done()
+    })
+    .catch(e => {
+      done(e)
+    })
+  })
+})
+
+
+// describe('refresh access token', function () {
+//   it('should refresh the access token received in the previous test', function (done) {
+//     // auth.refreshToken(cache.credentials, cache.token)
+//     auth.refreshToken(cache.credentials, cache.token)
+//     .then(rsp => {
+//       console.log('refreshed access token:', rsp)
+//       done()
+//     })
+//     .catch(e => {
+//       done(e)
+//     })
+//   })
+// })
