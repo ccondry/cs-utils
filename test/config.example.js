2627c1b84144e48e2bed44ed5d578d3539802506
Coty Condry <ccondry@cisco.com>
2018-06-06 14:09:30 -0500

    add example config file for mocha tests

---
+++ test/config.example.js
@@ -0,0 +1,18 @@
+const connectionDataString = `yourConnectionDataString`
+
+const orgEmail = 'youremail@domain.com'
+const orgPassword = 'yourOrgPassword'
+const orgId = 'your-org-id'
+const customerId = 'test-customer-id'
+const labMode = true
+const kmsScopes = true
+
+module.exports = {
+  connectionDataString,
+  orgEmail,
+  orgPassword,
+  orgId,
+  customerId,
+  labMode,
+  kmsScopes
+}
